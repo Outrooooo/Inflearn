a301c318e792f7ae980f8141329f0935223232cc
Jonghyeok, Ahn <sraed2@naver.com>
2025-03-14 17:51:37 +0900

    예제 5 프로젝트 준비

---
+++ Vue/5.가위바위보/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import RockScissorsPaper from '../src/components/RockScissorsPaper.vue'
+</script>
+
+<template>
+  <main>
+    <RockScissorsPaper />
+  </main>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
